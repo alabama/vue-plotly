317f580a34efc3de1698a95432d34fae072cabc5
Eugen Kandakov <kandakov@gmail.com>
2021-10-06 20:43:18 +0200

    replaced vue-cli build with vite to create an esm build

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import { createVuePlugin } from "vite-plugin-vue2";
+const path = require("path");
+
+module.exports = defineConfig({
+  plugins: [createVuePlugin({})],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/")
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "vue-plotly",
+      formats: ["es", "cjs", "umd"],
+      fileName: format => `vue-plotly.${format}.js`
+    },
+
+    emptyOutDir: true,
+    outDir: path.resolve(__dirname, "dist"),
+    assetsInlineLimit: 0,
+    polyfillModulePreload: true,
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue"
+        }
+      }
+    }
+  }
+});
